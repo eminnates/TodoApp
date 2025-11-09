5e90e0d175ef4893c3591e462b46c23c3207c4df
Emin <hakanmehmet849@gmail.com>
2025-11-09 23:55:00 +0300

    Add project files.

---
+++ TodoApp.API/Models/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TodoApp.API.Models
+{
+    public class AppUser : IdentityUser
+    {
+
+        public int? TodoPoints { get; set; } = 0;
+        public bool isActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ TodoApp.API/Models/Todos.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TodoApp.API.Models
+{
+    public class Todos
+    {
+        [Key]
+        public int TodoId { get; set; }
+        [ForeignKey("AppUser")]
+        public int UserId { get; set; }
+        [Required]
+        public string TodoContent { get; set; } = string.Empty;
+        public bool IsCompleted { get; set; } = false;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
