434abfab8b04398f439619914a0050e65d2335bd
Emin <hakanmehmet849@gmail.com>
2025-11-12 20:57:08 +0300

    DELETE unnecesary appuser validator

---
+++ TodoApp.API/Models/Validators/Auth/RegisterValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using TodoApp.API.DTOs.AuthDtos;
+
+namespace TodoApp.API.Models.Validators.Auth
+{
+    public class RegisterValidator : AbstractValidator<RegisterDto>
+    {
+        public RegisterValidator()
+        {
+            RuleFor(x => x.FullName)
+                .NotEmpty()
+                .MaximumLength(50);
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .MinimumLength(6)
+                .MaximumLength(100)
+                .ChildRules(password =>
+                {
+                    password.RuleFor(x => x)
+                        .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
+                        .Matches("[a-z]").WithMessage("Password must contain at least one lowercase letter.")
+                        .Matches("[0-9]").WithMessage("Password must contain at least one digit.")
+                        .Matches("[^a-zA-Z0-9]").WithMessage("Password must contain at least one special character.");
+                });
+        }
+    }
+}
